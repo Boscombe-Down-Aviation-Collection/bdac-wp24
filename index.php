3f10cb3468273e251679cdbbd6f6d54e3cc33a41
Oliver Berry <oliver-berry@live.co.uk>
2023-01-03 21:49:52 +0000

    set/index
    -> index.php:
      + comment
      + previous version of setting up the WP theme
    
    + templates/index.html
    
    + theme.json:
      + $schema
      + version

---
+++ index.php
@@ -0,0 +1,15 @@
+<!--
+  In production this file should be left blank
+-->
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <p>Hello world</p>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
