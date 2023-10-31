77d7230fba3688dbb95150a6cdc2cabc33cd8b4c
Oliver Berry <info@littlemonsterdigital.co.uk>
2023-10-31 21:45:18 +0000

    Enqueue stylesheets
    Add functions.php
    Add includes/front/enqueue.php

---
+++ includes/front/enqueue.php
@@ -0,0 +1,20 @@
+<?php 
+
+function dev23_enqueue() {
+    wp_register_style( 
+        'dev23_font_oswald_opens_sans', 
+        'https://fonts.googleapis.com/css2?family=Oswald:wght@400;600;700&family=Raleway:wght@400;600;700&display=swap', 
+    );
+    wp_register_style( 
+        'dev23_bootstrap_icons', 
+        get_theme_file_uri( '/assets/bootstrap-icons/bootstrap-icons.css' ),
+    );
+    wp_register_style( 
+        'dev23_theme', 
+        get_theme_file_uri( '/assets/public/index.css' ),
+    );
+
+    wp_enqueue_style( 'dev23_font_oswald_opens_sans' );
+    wp_enqueue_style( 'dev23_bootstrap_icons' );
+    wp_enqueue_style( 'dev23_theme' );
+} 
